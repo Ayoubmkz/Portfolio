782c493b5c23fcc73acbadcf5942133b4c7a3421
Ayoub MEKNOUZ <ayoubmeknouz.pro@gmail.com>
2024-11-18 17:26:18 +0100

    up

---
+++ script.js
@@ -0,0 +1,21 @@
+// import { neonCursor } from "https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js";
+
+// const root = document.getElementById("app");
+
+// const options = {
+// el: root,
+// shaderPoints: 16,
+// curvePoints: 80,
+
+
+
+// sleepRadiusY: 100,
+// sleepRadiusX: 100,
+
+// sleepTimeCoefx: 0.0025,
+// sleepTimeCoefY: 0.001,
+// };
+
+// neonCursor (options);
+
+
